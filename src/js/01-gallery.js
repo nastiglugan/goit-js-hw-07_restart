0ded8b48e5da652d8375aa7dbbce8d526872782d
NastiHluhan <0506167197more@gmail.com>
2024-11-22 23:50:21 +0200

    push

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ description, original, preview }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const imgId = evt.target.dataset.source;
+  const imgDesc = evt.target.alt;
+
+  const instance = basicLightbox.create(
+    `<img
+      class="gallery__image"
+      src="${imgId}"
+      alt="${imgDesc}"
+    />`,
+    {
+      // closable: false,
+    }
+  );
+
+  instance.show();
+}
